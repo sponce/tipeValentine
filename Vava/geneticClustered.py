0a9a2f3898dcad22a2d12b3a2717b7d81526ce76
Sebastien Ponce <Sebastien.Ponce@cern.ch>
2023-06-21 22:21:24 +0200

    Dropped unued City class

--- Vava/geneticClustered.py
+++ Vava/geneticClustered.py
@@ -3,20 +3,6 @@ from pylab import figure,plot,show
 import pickle
 import networkx
    
-class City:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    
-    def distance(self, city):
-        xDis = abs(self.x - city.x)
-        yDis = abs(self.y - city.y)
-        distance = np.sqrt((xDis ** 2) + (yDis ** 2))
-        return distance
-
-    def __repr__(self):
-        return "(" + str(self.x) + "," + str(self.y) + ")"
-
 class Fitness:
     def __init__(self, route):
         self.route = route
